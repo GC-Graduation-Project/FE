250bcfa48b953dd290acc17042392e43452b3ad5
LES0915 <silver_lives@naver.com>
2023-12-17 11:13:18 +0900

    다운로드까지

--- frontend/ios/Runner/GeneratedPluginRegistrant.m
+++ frontend/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,6 +6,12 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<image_gallery_saver/ImageGallerySaverPlugin.h>)
+#import <image_gallery_saver/ImageGallerySaverPlugin.h>
+#else
+@import image_gallery_saver;
+#endif
+
 #if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
 #import <image_picker_ios/FLTImagePickerPlugin.h>
 #else
@@ -15,6 +21,7 @@
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ImageGallerySaverPlugin registerWithRegistrar:[registry registrarForPlugin:@"ImageGallerySaverPlugin"]];
   [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
 }
 
--- frontend/lib/music.dart
+++ frontend/lib/music.dart
@@ -54,7 +54,8 @@ class AudioRecognitionScreen extends StatelessWidget {
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.only(top: 30, bottom: 30),
+                  margin: EdgeInsets.only(
+                      top: screenHeight * 0.03, bottom: screenHeight * 0.05),
                   width: screenWidth * 0.9,
                   height: screenHeight * 0.55,
                   decoration: BoxDecoration(
